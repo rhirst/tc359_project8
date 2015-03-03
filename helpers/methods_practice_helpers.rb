053328f242b31d1362711b3e196e768ebfbc6cdd
rhirst <r.hirst18@gmail.com>
2015-03-03 12:20:47 -0500

    completed image tag

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -4,6 +4,8 @@ module MethodsPracticeHelpers
     # Generate an image tag with the given source and the classes
     # "img-responsive" and "img-thumbnail". The image should also have an alt
     # attribute with the contents of alternate_text.
+
+    data = "<img class='img-responsive img-thumbnail' src='#{source}' alt='#{alternate_text}'>"
   end
 
   def bootstrap_alert(message, type='info')
@@ -11,12 +13,15 @@ module MethodsPracticeHelpers
     # to remember all HTML needed to do it right). It should show the message in
     # the "message" variable and should have a class appropriate for the "type"
     # that is passed in, defaulting to 'info' if no type is passed in.
+    alert = "<div class='alert alert-#{type}>#{message}</div>"
   end
 
   def current_date_and_time
     # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
     # Time.now.strftime is a great method chain you can use for this purpose.
     # See how to use it at: http://apidock.com/ruby/Time/strftime
+    time = Time.now
+    out_time = time.strftime("%B %d, %Y")
   end
 
   # ------------------------------------------
